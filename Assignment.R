162913e007881b43e94e3d6751f85826e2ebcf90
katherinelbrown91 <katherinelbrown91@gmail.com>
2017-10-26 21:08:05 -0600

    Part 1

---
+++ Assignment.R
@@ -0,0 +1,14 @@
+# Part 1. Introductions
+my.age <- 26
+my.name <- "Katie"
+MakeIntroduction <- function(my.name, my.age)
+  MakeIntroduction <- "Hello, my name is NAME and I am AGE years old."
+my.intro <- paste("Hello, my name is", my.name, "and I am", my.age, "years old.")
+my.intro
+casual.intro <- sub("Hello, my name is", "Hey I am", my.intro)
+library(stringr)
+capital.intro <- str_to_title(my.intro)
+capital.intro
+intro.e.count <- count.fields(my.intro, pattern = "e")
+intro.e.count <- str_count(my.intro, pattern = "e")
+intro.e.count
