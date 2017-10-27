6f1a768144fe3038c513cb997096c2ec4298d951
katherinelbrown91 <katherinelbrown91@gmail.com>
2017-10-26 21:22:25 -0600

    Part 2

--- Assignment.R
+++ Assignment.R
@@ -12,3 +12,15 @@ capital.intro
 intro.e.count <- count.fields(my.intro, pattern = "e")
 intro.e.count <- str_count(my.intro, pattern = "e")
 intro.e.count
+# Part 2. Books
+my.books <- c("To Say Nothing of the Dog", "Doomsday Book", "Alanna: The First Adventure", "I Capture the Castle", "Harry Potter and the Half-Blood Prince", "Dragonsinger")
+my.books
+top.three.books <- my.books[1:3]
+top.three.books
+book.reviews <- paste(my.books, "is a great read!")
+book.reviews
+books.without.four <- my.books[-4]
+books.without.four
+long.titles <- ifelse(my.books>15, yes = "this book has more than fifteen characters", no = "no book over 15")
+long.titles
+
